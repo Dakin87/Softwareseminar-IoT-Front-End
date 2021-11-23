f1d0fa78e2b8a0d85d6a7e869f59ac9191b821b2
Niklas <77452438+Dakin87@users.noreply.github.com>
2021-11-23 20:40:49 +0100

    Neuer Name

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Softwareseminar Thema 3</v-toolbar-title>
+      <v-toolbar-title>Softwareseminar IoT Front-End</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
