1143b2bb0e7d4e3c565a4750cb286a94d334c00b
Niklas <77452438+Dakin87@users.noreply.github.com>
2021-11-23 20:33:48 +0100

    init

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about pa-6">
+    <h1>Über diese Website</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis error
+      deleniti dolorem nostrum nemo earum maiores modi hic consectetur, labore
+      ipsa voluptates blanditiis repellendus soluta corrupti odio accusantium
+      aliquam quis.
+    </p>
+  </div>
+</template>
---
+++ src/views/Config.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about pa-6">
+    <h1>Konfigurator</h1>
+
+  </div>
+</template>
