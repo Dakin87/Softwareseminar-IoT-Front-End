3e9a07e838e9ba470af5582951fd6828dccee0d7
FritziQse <bt709387@uni-bayreuth.de>
2021-12-07 12:34:46 +0100

    Design Startseite

--- src/views/Startseite.vue
+++ src/views/Startseite.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Herzlich Willkommen bei MySmartHome"/>
-    <h1>Home</h1>
+  <div class="home pa-6">
+<h1> Startseite </h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
