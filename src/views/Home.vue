55153eedb706a522b749dec09c41ab4551873656
FritziQse <bt709387@uni-bayreuth.de>
2021-12-07 10:11:16 +0100

    Willkommensnachricht bearbeitet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Herzlich Willkommen bei MySmartHome"/>
     <h1>Home</h1>
   </div>
 </template>
