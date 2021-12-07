4f1c23583b1ea1a75eb73f1fb851decbf31bf14d
FritziQse <bt709387@uni-bayreuth.de>
2021-12-07 12:31:35 +0100

    Home zu Startseite umbenannt

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Startseite from '../views/Startseite.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Startseite',
+    component: Startseite
   },
   
   {
