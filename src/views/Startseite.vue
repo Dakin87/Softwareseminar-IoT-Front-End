2cf316e86eb5c1f9f8292856bc49aead0851234a
FritziQse <bt709387@uni-bayreuth.de>
2021-12-10 14:21:59 +0100

    Hochladen neuer Bilder für die Startseite

--- src/views/Startseite.vue
+++ src/views/Startseite.vue
@@ -5,67 +5,68 @@
     class="fill-height"
     fluid
     style="min-height: 434px"
-  >
-    <v-fade-transition mode="out-in">
-      <v-row>
-        <v-col cols="6">
-          <v-card>
-            <v-img
-              src="SmartHome.jpg"
-              height="125"
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              Ihr Traumhaus:
-            </v-card-title>
-          </v-card>
-        </v-col>
+   >
+     <v-fade-transition mode="out-in">
+       <v-row>
+         <v-col cols="6">
+           <v-card>
+             <v-img
+               src="SmartHome.jpg"
+               height="125"
+               class="grey darken-4"
+             ></v-img>
+             <v-card-title class="text-h6">
+               Ihr Traumhaus:
+             </v-card-title>
+           </v-card>
+         </v-col>
 
-        <v-col cols="6">
-          <v-card>
-            <v-img
-              src="Touchpad.jpg"
-              height="125"
-              contain
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              persönlich,
-            </v-card-title>
-          </v-card>
-        </v-col>
+         <v-col cols="6">
+           <v-card>
+             <v-img
+               src="persönlich.jpg"
+               height="125"
+               contain
+               class="grey darken-4"
+             ></v-img>
+             <v-card-title class="text-h6">
+               persönlich,
+             </v-card-title>
+           </v-card>
+         </v-col>
 
-        <v-col cols="6">
-          <v-card>
-            <v-img
-              src="Touchpad.jpg"
-              max-height="125"
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              einfach,
-            </v-card-title>
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card>
-            <v-img
-              src="Touchpad.jpg"
-              max-height="125"
-              contain
-              class="grey darken-4"
-            ></v-img>
-            <v-card-title class="text-h6">
-              smart !
-            </v-card-title>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-fade-transition>
-  </v-container>
+         <v-col cols="6">
+           <v-card>
+             <v-img
+               src="Kind_Handy.jpg"
+               max-height="125"
+               class="grey darken-4"
+             ></v-img>
+             <v-card-title class="text-h6">
+               einfach,
+             </v-card-title>
+           </v-card>
+         </v-col>
+  
+         <v-col cols="6">
+           <v-card>
+             <v-img
+               src="Touchpad.jpg"
+               max-height="125"
+               contain
+                class="grey darken-4"
+             ></v-img>
+             <v-card-title class="text-h6">
+               smart !
+             </v-card-title>
+           </v-card>
+         </v-col>
+       </v-row>
+      </v-fade-transition>
+   </v-container>
 
   </div>
+
 </template>
 
 <script>
