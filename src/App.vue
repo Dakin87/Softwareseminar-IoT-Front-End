2274ab922f1da19cfa39678ed797a7195f6de885
Niklas <77452438+Dakin87@users.noreply.github.com>
2021-12-12 16:57:26 +0100

    Fix: Inhalte wieder sichtbar

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,101 @@
-<template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      dark
-      class="secondary"
-    >
-    {{ /* dark regelt den dark mode, und stellt automatisch die Textfarbe auf hell */ }}
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Navigation
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            MySmartHome
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
+  <template>
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        dark
+        class="secondary"
       >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
+      {{ /* dark regelt den dark mode, und stellt automatisch die Textfarbe auf hell */ }}
+        <v-list-item>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="text-h6">
+              Navgiation
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              MySmartHome
+            </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-     <v-card class="overflow-hidden">
-    <v-app-bar
+        <v-divider></v-divider>
+        <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            :to="item.to"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar 
       app
       dark
       shrink-on-scroll
       src="SmartHome.jpg"
       scroll-target="#scrolling-techniques-2"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
+
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>MySmartHome</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text to='/login'>Login</v-btn>
+      </v-app-bar>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+      <v-footer
+      color="primary"
+      dark
+      padless
+      app
     >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-        <v-app-bar-title>
-         MySmartHome
-        </v-app-bar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn text to='/login'>Login</v-btn>
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-2"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
-  </v-card>
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+        >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        <v-col
+          class="primary py-1 text-center white--text"
+          cols="12"
+        >
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
   
 </template>
-
 <script>
   export default {
     data: () => ({
       drawer: null,
       items: [
-          { title: 'Startseite', icon: 'mdi-home-circle', to: '/'},
+          { title: 'Startseite', icon: 'mdi-home', to: '/'},
           { title: 'Konfigurator', icon: 'mdi-view-dashboard', to: '/config'},
           { title: 'Über', icon: 'mdi-help-box', to: '/about'},
         ],
