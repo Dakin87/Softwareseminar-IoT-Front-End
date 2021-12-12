619ced341e6fe7ba7a18529173ec16f01931ae5d
FritziQse <bt709387@uni-bayreuth.de>
2021-12-12 14:07:02 +0100

    Änderung am Code für das Design der Startseite

--- src/views/Startseite.vue
+++ src/views/Startseite.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="home pa-6">
 
-<v-container fluid>
-    <v-row justify="space-around">
-      <v-col cols="5">
-        <div class="text-h6 mb-1">
-          Ihr Traumhaus:
-        </div>
-        <div class="subheading">
-          persönlich,
-        </div>
-        <v-img
-          src="persönlich.jpg"
-        ></v-img>
-        <div class="subheading pt-4">
-          einfach,
-        </div>
-        <v-img
-          src="Kind_Handy.jpg"
-        ></v-img>
-        <div class="subheading pt-4">
-          smart !
-        </div>
-        <v-img
-          src="Touchpad.jpg"
-        ></v-img>
-      </v-col>
-    </v-row>
-  </v-container>
+   <v-container fluid>
+     <v-row justify="space-around">
+       <v-col cols="5">
+         <div class="text-h6 mb-1">
+           Ihr Traumhaus:
+         </div>
+         <div class="subheading">
+           persönlich,
+         </div>
+         <v-img
+           src="persönlich.jpg"
+         ></v-img>
+         <div class="subheading pt-4">
+           einfach,
+         </div>
+         <v-img
+           src="Kind_Handy.jpg"
+         ></v-img>
+         <div class="subheading pt-4">
+           smart !
+         </div>
+         <v-img
+           src="Touchpad.jpg"
+         ></v-img>
+       </v-col>
+     </v-row>
+   </v-container>
 
   </div>
 
