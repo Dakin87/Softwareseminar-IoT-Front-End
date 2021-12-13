47c49c6253cf8367533d911c26da6638b19925d8
FritziQse <bt709387@uni-bayreuth.de>
2021-12-13 12:33:59 +0100

    Änderung Text About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="about pa-6">
-    <h1>Über diese Website</h1>
-    <p1>
-     MySmartHome ermöglicht es ihnen
-     persönlich, einfach, smart
-     ihr Traumhaus zu konfigurieren !
-    </p1>
-    <p2>
-     Wählen sie dafür die Geräte aus, die vernetzt werden sollen und wir zeigen ihnen, 
-     wie diese miteinander kombinierbar sind. 
+  <div class="about pa-6" >
+    <h1>Wer wir sind und wofür wir stehen</h1>
 
+    <p>
+
+    </p>
+
+    <p class= "font-weight-bold">
+     MySmartHome - persönlich, einfach, smart !
+    </p>
+
+    <p class= "font-weight-regular">
+     Wir ermöglichen es ihnen -auch ohne Vorkenntnisse jeglicher technischer Art- in kürzster Zeit ihr Zuhause zum Smarthome zu machen!
+    </p>
+
+    <p class= "font-weight-regular">
+     Wählen sie dafür einfach die Geräte aus, die vernetzt werden sollen und wir zeigen ihnen, 
+     wie diese miteinander kombinierbar sind. 
+    </p>
+
+    <p class= "font-weight-regular">
      Dadurch lässt sich die Anzahl der benötigten Apps minimieren und vor Allem können Inkompatibilitäten 
      zwischen Geräten schon im Voraus feststgestellt und somit vermieden werden. 
+    </p>
 
-     Wir ermöglichen es ihnen -auch ohne Vorkenntnisse jeglicher technischer Art- in kürzster Zeit ihr Traumhaus zu vernetzen!
-    </p2>
   </div>
 </template>
 
