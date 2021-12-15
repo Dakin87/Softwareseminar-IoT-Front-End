084137afc81c229a19386ad075a32f45bee264fd
FritziQse <bt709387@uni-bayreuth.de>
2021-12-15 13:42:45 +0100

    Inhalt About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,72 @@
 <template>
   <div class="about pa-6" >
-    <h1>Wer wir sind und wofür wir stehen</h1>
+    <h1>Impressum</h1>
+
+    <h3>I. Kundensupport</h3>
+
+    <p> 
+      Telefon: +49 12 345 67890 (Montag -Freitag, 8.00 Uhr – 20.00 Uhr)
+      Email: support@mysmarthome.com
+    </p>
+      
+    <p>Verantwortlich für den Inhalt der Seite: </p> 
 
     <p>
-
+      MySmartHome GmbH
+      Musterstraße 1
+      12345 Musterstadt, Deutschland
     </p>
 
-    <p class= "font-weight-bold">
-     MySmartHome - persönlich, einfach, smart !
+    <p>Kontoverbindung</p>
+
+    <p>
+     Firma: MySmartHome GmbH
+     Bank: Deutsche Bank
+     IBAN: DE12  3456 7890 12
+     BIC: ABCDEFG
     </p>
 
-    <p class= "font-weight-regular">
-     Wir ermöglichen es ihnen -auch ohne Vorkenntnisse jeglicher technischer Art- in kürzster Zeit ihr Zuhause zum Smarthome zu machen!
+    <p>
+     Geschäftsführer:
+     Friederike Kuhse, Niklas Karl
     </p>
 
-    <p class= "font-weight-regular">
-     Wählen sie dafür einfach die Geräte aus, die vernetzt werden sollen und wir zeigen ihnen, 
-     wie diese miteinander kombinierbar sind. 
+    <p>
+     Handelsregister: Amtsgericht Musterstadt, HRB 123456 M
     </p>
 
-    <p class= "font-weight-regular">
-     Dadurch lässt sich die Anzahl der benötigten Apps minimieren und vor Allem können Inkompatibilitäten 
-     zwischen Geräten schon im Voraus feststgestellt und somit vermieden werden. 
+    <p>
+     Umsatzsteuer-Identifikationsnummer: DE 123456789
     </p>
+
+    <p>
+     Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit, die Sie hier finden https://ec.europa.eu/consumers/odr/.
+     Zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle sind wir nicht verpflichtet und nicht bereit.
+   </p>
+
+   <h3>II. Name und Anschrift des Datenschutzbeauftragten</h3>
+   
+   <p>
+     Wenn Sie weitere Fragen hinsichtlich der Verarbeitung Ihrer personenbezogenen Daten haben, können Sie sich direkt an unseren Beauftragten für den Datenschutz wenden, der auch im Falle von Auskunftsersuchen, Anträgen oder Beschwerden zur Verfügung steht:
+   </p>
+
+   <p>
+     Muster GmbH
+     Musterstraße 2
+     12345 Musterstadt
+     Deutschland
+     www.mustergmbh.de
+   </p>
+
+    <h3>III. Presseanfragen</h3>
+
+   <p> 
+     Für Presseanfragen melden Sie sich gern bei uns unter: presse@mysmarthome.com
+   </p>
+
+   <h4>
+     Bei dieser Website handelt es sich um eine Seminararbeit im Rahmen des Softwareprojektseminars der Universtität Bayreuth. Alle Inhalte sind in Rahmen dessen frei erfunden!
+   </h4>
 
   </div>
 </template>
