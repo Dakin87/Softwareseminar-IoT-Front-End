c17fbe38eeed6cdd7056ccb333df01ad8402a1d7
FritziQse <bt709387@uni-bayreuth.de>
2021-12-19 15:05:47 +0100

    Text Startseite

--- src/views/Startseite.vue
+++ src/views/Startseite.vue
@@ -79,10 +79,28 @@
 
         <v-col cols="12">    
           <v-card>
+           <h3>MySmartHome - Ihr Smarthome Konfigurator</h3>
            <p>
-             Test Zeile 1
+             <br>Smarte Geräte können ihren Nutzern das Leben in vielen Bereichen erleichtern. 
+             Selbst Einsteigermodelle beherrschen Dinge wie das Steuern von Lichtern oder das Ein- und Ausschalten von Geräten für die Wohnzimmerunterhaltung. 
+             Fortgeschrittene Systeme eignen sich darüber hinaus auch für die erweiterte Hausautomation.
+             Bei uns erwartet Sie ein vielfältiges Sortiment an Smart Home Geräten. 
+             Mit unseren energiesparenden Thermostaten können Sie so einfach wie noch nie ihre Heizkosten senken. 
+             Sprachassistenten erlauben eine komfortable Steuerung Ihrer Beleuchtung, Rollläden oder Kaffeemaschinen. 
              <br>
-             Test Zeile 2
+             <br>
+           </p>
+           <p class="font-weight-bold">
+            Was es beim Kauf der modernen Geräte zu beachten gilt:
+           </p>
+           <p> 
+             Der Markt hält mittlerweile eine große Auswahl an Produkten für die smarte und preiswerte Haussteuerung bereit, weshalb es nicht einfach ist, auf Anhieb die richtige Lösung zu finden. 
+             Angesichts der Vielzahl an Produkten ist es ratsam, sich Gedanken über die geplante Nutzung zu machen und die Geräte einem Preisvergleich zu unterziehen aber auch nachzusehen welche Geräte wie miteinander kombinierbar sind.
+             Oft benötigen Bewohner eines Smarthomes mehrere Apps um alle Geräte steuern zu können, was die Nutzung oft kompliziert und umständlich macht.
+             Mithilfe unseres Konfigurators können sie nun die Möglichkeiten ihres Zuhauses optimal auszunutzen und ihre perfekte Smarthome Lösung erstellen.
+             Dabei können sie sehen, welche Kombinationen sich am besten für ihr Zuhause eignen beziehungsweise eher ungeeignet sind und alle Geräte über ein einziges Steuerungselement nutzen. 
+             So kann sich jedermann seinen Traum vom eigenen Smarthome erfüllen - auch ohne viele verschiedene Steuerungselemente und technisches Vorwissen.
+             <br>
              </p>
           </v-card>
         </v-col>
