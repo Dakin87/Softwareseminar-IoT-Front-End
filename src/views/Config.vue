89b7d12f6f1c4cad4bab6049275650482e61dc04
Niklas <77452438+Dakin87@users.noreply.github.com>
2021-12-19 22:19:35 +0100

    Deletable items, clear button, groups for items

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,65 +1,67 @@
 <template>
+  
   <div class="row 
     px-5 py-3 mx-10 my-5 justify-center
     elevation-5
     ">
+    
     <div class="col-3
     elevation-1"
     >
-      <h3>Draggable 1</h3>
-      <draggable class="list-group" :list="list1" group="people" @change="log" :move="checkMove">
-        <div
+      <h3>Sensoren</h3>
+      <draggable class="list-group" :list="list1" :group="{name: 'Sensoren', pull: 'clone', put: false}" @change="log">
+        <v-chip
           class="list-group-item"
-          v-for="(element, index) in list1"
+          v-for="(element) in list1"
           :key="element.name"
         >
-          {{ element.name }} ID:{{element.id}} {{ index }}
-        </div>
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
       </draggable>
     </div>
 
     <div class="col-3
     elevation-1
     ">
-      <h3>Draggable 2</h3>
-      <draggable class="list-group" :list="list2" :group="{name: 'people', pull: 'clone', put: false}" @change="log">
-        <div
+      <h3>Interaktion</h3>
+      <draggable class="list-group" :list="list2" :group="{name: 'Interaktion', pull: 'clone', put: false}" @change="log">
+        <v-chip
           class="list-group-item"
-          v-for="(element, index) in list2"
+          v-for="(element) in list2"
           :key="element.name"
         >
-          {{ element.name }} ID:{{element.id}} {{ index }}
-        </div>
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
       </draggable>
     </div>
 
     <div class="col-3
     elevation-1
     ">
-      <h3>Draggable 3</h3>
-      <draggable class="list-group" :list="list3" group="people" @change="log">
-        <div
+      <h3>Daten</h3>
+      <draggable class="list-group" :list="list3" :group="{name: 'Daten', pull: 'clone', put: false}" @change="log">
+        <v-chip
           class="list-group-item"
-          v-for="(element, index) in list3"
+          v-for="(element) in list3"
           :key="element.name"
         >
-          {{ element.name }} ID:{{element.id}} {{ index }}
-        </div>
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
       </draggable>
     </div>
 
     <div class="col-3
     elevation-1
     ">
-      <h3>Draggable 4</h3>
-      <draggable class="list-group" :list="list4" group="people" @change="log">
-        <div
+      <h3>Service</h3>
+      <draggable class="list-group" :list="list4" :group="{name: 'Service', pull: 'clone', put: false}" @change="log">
+        <v-chip
           class="list-group-item"
-          v-for="(element, index) in list4"
+          v-for="(element) in list4"
           :key="element.name"
         >
-          {{ element.name }} ID:{{element.id}} {{ index }} 
-        </div>
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
       </draggable>
     </div>
 
@@ -71,22 +73,83 @@
 
     <rawDisplayer class="col-3" :value="list4" title="List 4" />
 
-  <div class="col-12
+  <div class="col-3
     elevation-1"
     >
-      <h3>Gewünschte Komponenten</h3>
-      <draggable class="list-group" :list="list5" group="people" @change="log">
-        <div
+      <h3>Gewünschte Sensoren</h3>
+      <draggable class="list-group" :list="list5" group="Sensoren" @change="log">
+        <v-chip
           class="list-group-item"
-          v-for="(element, index) in list5"
+          v-for="(element) in list5"
           :key="element.name"
+          @click="removeAt(list5, element)"
         >
-          {{ element.name }} ID:{{element.id}} {{ index }} 
-        </div>
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
       </draggable>
     </div>
 
-    <rawDisplayer class="col-12" :value="list5" title="List 5" />
+    <div class="col-3
+    elevation-1"
+    >
+      <h3>Gewünschte Interaktion</h3>
+      <draggable class="list-group" :list="list6" group="Interaktion" @change="log">
+        <v-chip
+          class="list-group-item"
+          v-for="(element) in list6"
+          :key="element.name"
+          @click="removeAt(list6, element)"
+        >
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
+      </draggable>
+    </div>
+
+    <div class="col-3
+    elevation-1"
+    >
+      <h3>Gewünschte Daten</h3>
+      <draggable class="list-group" :list="list7" group="Daten" @change="log">
+        <v-chip
+          class="list-group-item"
+          v-for="(element) in list7"
+          :key="element.name"
+          @click="removeAt(list7, element)"
+        >
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
+      </draggable>
+    </div>
+
+    <div class="col-3
+    elevation-1"
+    >
+      <h3>Gewünschter Service</h3>
+      <draggable class="list-group" :list="list8" group="Service" @change="log">
+        <v-chip
+          class="list-group-item"
+          v-for="(element) in list8"
+          :key="element.name"
+          @click="removeAt(list8, element)"
+        >
+          {{ element.name }} ID:{{element.id}}
+        </v-chip>
+      </draggable>
+    </div>
+
+    <rawDisplayer class="col-3" :value="list5" title="List 5" />
+    <v-spacer></v-spacer>
+
+    <div class="col-12
+    elevation-1"
+    justify-center
+    >
+      <v-btn
+      elevation="2"
+      @click="clearAll"
+    > Zurücksetzen
+    </v-btn>
+    </div>
 
   </div>
 </template>
@@ -125,9 +188,16 @@
           { name: "Johnson", id: 13 }
         ],
         list5: [
-          { name: "Horst", id: 14 },
-          { name: "Edgard", id: 15 },
-          { name: "Johnson", id: 16 }
+        
+        ],
+        list6: [
+        
+        ],
+        list7: [
+        
+        ],
+        list8: [
+        
         ]
       };
     },
@@ -150,6 +220,15 @@
         if (evt.relatedContext.list.length>3){
 				return false
 			}
+      },
+      removeAt: function(list, index) {
+        list.splice(index, 1);
+      },
+      clearAll: function(){
+        this.$data.list5 = [];
+        this.$data.list6 = [];
+        this.$data.list7 = [];
+        this.$data.list8 = [];
       }
     }
   };
