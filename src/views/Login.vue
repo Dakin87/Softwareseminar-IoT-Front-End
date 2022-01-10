36b7b8f7218c3552cf173373521f51c66bbd4e14
FritziQse <bt709387@uni-bayreuth.de>
2022-01-10 13:03:21 +0100

    Registrier Knopf

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,27 @@
 <template>
   <div>
     <v-content>
-      <v-card 
-        width="500" 
-        class="pa-3 mx-auto"
-        elevation="7">
-        <v-card-title class="justify-center">Anmeldung</v-card-title>
+      <v-card width="500" class="mx-auto mt-9">
+        <v-card-title>Anmeldung</v-card-title>
         <v-card-text>
-          <v-form v-model="valid">
-            <v-text-field
-              v-model="name" 
-              label="Benutzername" 
-              prepend-icon="mdi-account"
-              type="text"
-              :rules="nameRules"
-              required
-              />
-            <v-text-field 
-              v-model="password"
-              label="Passwort" 
-              prepend-icon="mdi-lock" 
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword=!showPassword"
-              :rules="passwordRules"
-              required
-              />
-          </v-form>
+          <v-text-field label="Benutzername" prepend-icon="mdi-account-circle"/>
+          <v-text-field 
+          label="Passwort" 
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"/>
         </v-card-text>
-        <v-spacer></v-spacer>
-        <v-card-actions  class="justify-center">
-          <v-btn color="primary" :disabled="!valid" to='/config'>Login</v-btn>
+
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-btn color="info">Login</v-btn>
         </v-card-actions>
       </v-card>
     </v-content>
-  </div>
+
+ </div>
   
 </template>
 
